b1d4dabbe09fb5d7fe1a893ab351387f4490cb5e
annapacz <anna.paczkowska@prowly.com>
2025-07-10 09:35:49 +0100

    Delete playwright.storybook-snapshot.config.ts
    Signed-off-by: annapacz <anna.paczkowska@prowly.com>

--- playwright.storybook-snapshot.config.ts
+++
@@ -1,8 +0,0 @@
-import defaultConfig from './playwright.storybook.config';
-
-const config = {
-  ...defaultConfig,
-  testMatch: /\.storybook-snapshot-test.ts(x){0,1}$/,
-};
-
-export default config;
